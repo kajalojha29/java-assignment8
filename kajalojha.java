264f3c6837f08343b93c4296136eba077184556b
kajalojha29 <109940301+kajalojha29@users.noreply.github.com>
2022-07-26 11:26:27 +0530

    Create kajalojha.java

---
+++ kajalojha.java
@@ -0,0 +1,95 @@
+//Q1
+//class Name {
+//public static void main(Strings args[])
+//Name object = new Name ();
+//obj.hobby();
+//String name = kajal ojha ;
+//System.out.println("name");
+//int age = 19 ;
+//System.out.println( age );
+//}
+ //string void(hobby){
+//	System.out.println("my hobby is listing to music");
+	
+//}
+
+
+
+
+
+//}
+
+
+//Q4
+//class Calci{
+	//public static void main (String args[]){
+		//addy();
+		//subby();
+		//Calci Working Obj = new calci Worning();
+		//obj.diviji();
+		//obj.mull();
+		//}
+	
+	//class calci{
+		
+		//Static void addy (){
+			//System.out.println("addy");
+		//}
+		//Static void subby(){
+			
+			//System.out.println("subby");
+		//}
+		 //void  diviji(){
+			//System.out.println("diviji");
+			
+		//}
+		//void mull(){
+			//System.out.println("mull");
+		//}
+	//}
+	//}
+
+
+
+
+//Q5
+
+//WRAPPER CLASSE : Wrapper classe are use to make java fully object oriented . In which we use data type like int , float , char ,double etc .In which we use 
+               //  primitive data types . In which mainly two concepts are using :
+				// (1)= boxing (eg integer x = 9);
+				// (2)= unboxing ( eg int x = y )
+			
+//Q2 
+
+   
+   //Class name = class Name , class Java 
+	//function name = myFunction() 
+	//variable  = new_Variable 
+	//static variable  = Static  My_Class
+	
+	
+//Q3
+
+//1(a) .java file = .javafile is a run file .	
+//	(b) .class in java = when we write a code first of all we compile our code . we compile our code  by .class in java .		
+				 
+
+//(b) Java is not fully object oriented programming language because in java there is primitive data types like int , float , char , double , boolean , long ,
+ //short , byte due to presence of primitive data types java is not fully object oriented programming language.
+
+
+//(c)  NR - WA 
+// int void ( int a , int b ){
+	//int sum  =a+b ;
+	// System.out.println(sum);
+//}
+
+
+
+
+
+
+
+
+
+
