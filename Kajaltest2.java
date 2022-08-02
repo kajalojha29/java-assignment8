e6e778ff9ffb4d4468ca747a16c2da6ad9567d1b
kajalojha29 <109940301+kajalojha29@users.noreply.github.com>
2022-08-02 11:48:31 +0530

    Create Kajaltest2.java
    javatest2 submitted

---
+++ Kajaltest2.java
@@ -0,0 +1,110 @@
+//import java.util.Scanner;
+//class Test{
+//public static void main(String args[]){
+//Scanner sc = new Scanner(System.in);
+ //Q2
+ //int i = sc.nextInt();
+//if( i%2==0){
+//System.out.println("even");
+//}else{
+//System.out.println("odd");
+//}
+
+//Q2
+//int a = sc.nextInt();
+//int i = 55 ; 
+//if(a>55){
+	//System.out.println("Number is greater");
+//}else{
+	//System.out.println(";umber is not greater");
+//}
+
+//Q3
+//int a = sc.nextInt();
+//int b = sc.nextInt();
+//if(a==b){
+	//System.out.println("Number is equal");
+//}else if (a<b){
+	//System.out.println("b is greater ");
+//}else if (a>b){
+	//System.out.println("a is greater");
+//}else{
+	//System.out.println("number is not equal");
+//}
+
+//Q6
+//int a = sc.nextInt();
+
+//for(int i = 1 ; i<=a ;i++){
+	//if(a == 6){
+		//continue;
+	//}
+	//System.out.println(i);
+	//}
+
+//Q5 
+
+// int rollNo;
+// rollNo = sc.nextInt(); 
+//switch(rollNo )
+	//{
+	//case 579 :
+	//System.out.println("Sonu roll no");
+	//break;
+	//case 546:
+	//System.out.println("Monu roll no");
+	//break;
+	//case 553:
+	//System.out.println("Manu roll no");
+	//break;
+	//case 544:
+	//System.out.println("Meenu  roll no");
+	//break;
+    //case 516:
+	//System.out.println("Meenu roll no");
+	//break;
+	//default:
+   //System.out.println("Not Allowed");
+   //break;
+   
+   //}
+	
+//Q8
+//int n = 3;
+//int m = 5;
+//for(int i = 1 ; i<=n ;i++){
+	//for(int j = 1 ; j<=m ; j++){
+		//if(i%2==0 && j%2==0 || i%2==1 && j%2==1 ){
+			//System.out.print("1");
+		//}else{
+			//System.out.print("0");
+		//}
+		
+	//}
+	//System.out.println();
+	
+	//Q5
+	//Pass by value = Java only support pass by value . In pass by value let we have a notes of eng
+	//and we give copy of notes to someone then if he change somthing in notes than it does not 
+	//effect on original notes . thats why java support pass by value .
+	
+	//Pass by refernce = java does not support pass by refernce beacuse anyone can change something
+	//and it affect  and in pointer are use . pointer give the address of data and anyone can change 
+	//the data .
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//}
+
+//}
+
+
